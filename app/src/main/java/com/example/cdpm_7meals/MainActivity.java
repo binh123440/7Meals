744fd9b8415e6d3c3080922bd08461d0e148999e
binh123440 <binhgolden123@gmail.com>
2023-10-02 09:46:38 +0700

    first commit

---
+++ app/src/main/java/com/example/cdpm_7meals/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cdpm_7meals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
